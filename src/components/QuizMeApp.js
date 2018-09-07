ee179f71820ed5a57c71c91b6b665f952da1ea22
Abel Trotter <a_trotter0@hotmail.com>
2018-09-07 09:44:19 -0700

    Add HashRouter, Switch, and intial Routes to app, add Card and Home components

--- src/components/QuizMeApp.js
+++ src/components/QuizMeApp.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import '../styles/QuizMeApp.css';
 import Nav from './Nav';
+import Home from './Home';
+import Cards from './Cards';
 
 class QuizMeApp extends Component {
   render() {
     return (
       <div>
         <Nav />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/html-css' key='html-css' component={Cards} />
+        </Switch>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import QuizMeApp from './components/QuizMeApp';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <QuizMeApp />,
+  <HashRouter>
+    <QuizMeApp />
+  </HashRouter>,
   document.getElementById('root')
 );
 registerServiceWorker();
